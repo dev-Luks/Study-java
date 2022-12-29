40a7a7373a90cb7e21aea0b4c42688e05a76eb69
dev-luk <rladnrtjdsla@gmail.com>
2022-12-29 17:40:01 +0900

    2022년 12월 29일 print, variable

---
+++ workspace/print/src/printtest/PrintTest2.java
@@ -0,0 +1,22 @@
+package printtest;
+
+public class PrintTest2 {
+
+	public static void main(String[] args) {
+
+//		제어문자
+//		반드시 따옴표 안에서 작성한다.
+		
+//		\n : new line, 줄바꿈, 개행문자
+//		\t : tab, 위 아래 줄 간격 맞춰 띄기
+//		\" : " 표현
+//		\' : ' 표현
+//		\\ : \ 표현
+		
+//		System.out.print("\"김욱성\"\n");
+		System.out.println("김욱성");
+		System.out.print("20살");
+
+	}
+
+}
---
+++ workspace/variable/src/variableTest/variable.java
@@ -0,0 +1,20 @@
+package variableTest;
+
+public class variable {
+
+	public static void main(String[] args) {
+		int age = 10;
+		float interestRate = 3.55f;
+		double compoundInterest = 3.55;
+		char grade = 'A';
+		String data = "ABC";
+		
+		System.out.println(age);
+		System.out.println(interestRate);
+		System.out.println(compoundInterest);
+		System.out.println(grade);
+		System.out.println(data);
+		
+	}
+
+}
